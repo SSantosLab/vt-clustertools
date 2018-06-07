982d23047990aeb7e27205b9912e0bdd5b152af8
Connor <connor.burgad@gmail.com>
2018-06-07 14:12:11 -0500

    updated to des18a

---
+++ Memb-assign/gmmsetups.sh
@@ -0,0 +1,4 @@
+export PYTHONPATH=/home/s1/bwelch/new_sklearn/:/home/s1/bwelch/esutil/lib/python2.7/site-packages:$PYTHONPATH
+#source activate $CONDA_DIR/envs/des17a
+#source activate default
+source activate des18a
